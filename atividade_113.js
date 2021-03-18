ad0ce22a784472d437d9e7af1f55ca610b571d5a
Lucas Leite <70826073+leitecsleite@users.noreply.github.com>
2021-03-18 13:01:19 -0300

    exercicio

---
+++ atividade_113.js
@@ -0,0 +1,14 @@
+var numero_1 ="21"; 
+var numero_2 = 21; 
+
+if(numero_1 == numero_2) {
+    console.log("São iguais"); 
+}else{
+    console.log('São diferentes');
+}
+
+if ( numero_1 === numero_2 ) {
+    console.log("são iguais");
+} else {
+    console.log("são diferentes");
+}
