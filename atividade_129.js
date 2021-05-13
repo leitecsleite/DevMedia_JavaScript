dfb2cc61ec6b59dfaa8b4f21c873b35090b0cb90
Lucas Leite <carvalho1104@hotmail.com>
2021-05-12 21:58:09 -0300

    Create atividade_129.js

---
+++ atividade_129.js
@@ -0,0 +1,9 @@
+
+let num1 = 10.54367 // number ; 
+let num2 = 2.5 // Number; 
+
+console.log(num1.toFixed(1)); 
+console.log(num1.toString() + num2);
+
+console.log(Number.isInteger(num1)); // Esse número é inteiro ?
+console.log(Number.isNaN(num2)); 
